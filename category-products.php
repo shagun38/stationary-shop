c5a47df05ca8d948b7f9ba49eb8fab5a51f95159
Shagun <shagun22r@gmail.com>
2025-05-16 23:37:38 +0530

    alt , meta tag
    add alt name for image , and meta tags

--- category-products.php
+++ category-products.php
@@ -13,6 +13,10 @@ if (!$category) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="description" content="Your one-stop Stationery Shop for writing supplies, paper products, art & craft materials, and office essentials. Best prices and fast delivery.">
+    <meta name="keywords" content="stationery, writing supplies, paper, office, art, craft, shop online">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Products in <?php echo htmlspecialchars($category); ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
@@ -57,7 +61,7 @@ if (!$category) {
                             <td>{$row['name']}</td>
                             <td>{$row['description']}</td>
                             <td>{$row['price']}</td>
-                            <td><img src='{$row['image']}' style='max-height:50px;'></td>
+                            <td><img src='{$row['image']}' alt='{$row['name']}' style='max-height:50px;'></td>
                             <td>
                                 <a class='action-link' href='edit-product.php?id={$row['id']}'>Edit</a>
                                 <a class='action-link' href='delete-product.php?id={$row['id']}' onclick=\"return confirm('Are you sure you want to delete this product?')\">Delete</a>
--- products.php
+++ products.php
@@ -6,6 +6,10 @@ include 'db.php';
 <html lang="en">
 <head>
 <meta charset="UTF-8">
+<meta name="description" content="Your one-stop Stationery Shop for writing supplies, paper products, art & craft materials, and office essentials. Best prices and fast delivery.">
+<meta name="keywords" content="stationery, writing supplies, paper, office, art, craft, shop online">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Admin Dashboard - Stationery Shop</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
@@ -55,7 +59,7 @@ include 'db.php';
                         <td>{$row['price']}</td>
                         <td>{$row['category']}</td>
 
-                        <td><img src='{$row['image']}' style='max-height:50px;'></td>
+                        <td><img src='{$row['image']}' alt='{$row['name']}' style='max-height:50px;'></td>
                         <td>
                             <a class='action-link' href='edit-product.php?id={$row['id']}'>Edit</a>
                             <a class='action-link' href='delete-product.php?id={$row['id']}' onclick=\"return confirm('Are you sure you want to delete this product?')\">Delete</a>
